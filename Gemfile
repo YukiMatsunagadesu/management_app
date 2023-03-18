5493aa5e01ab559c131147cfb35301a2cc82842b
YukiMatsunagadesu <toshiya1048.reon@icloud.com>
2023-03-18 19:34:37 +0900

    カレンダー作ってmemberページのスタイリングした

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
 
+gem "simple_calendar", "~> 2.4"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.3"
 
@@ -48,6 +49,7 @@ gem "bootsnap", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
@@ -70,3 +72,5 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+
--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -8,6 +8,7 @@ class MemberController < ApplicationController
   def calender
     @id=params[:id]
     @member=Member.find(params[:id])
+    
   end
   def application
     @id=params[:id]
@@ -16,13 +17,12 @@ class MemberController < ApplicationController
   def attendance_management
     @id=params[:id]
     @member=Member.find(params[:id])
-    @start_date = Date.current.beginning_of_month
-    @end_date = Date.current.end_of_month
 
   end
   def profile_edit
     @id=params[:id]
     @member=Member.find(params[:id])
+    
   end
   def profile_update
     @id=params[:id]
@@ -31,29 +31,33 @@ class MemberController < ApplicationController
     @member.mail=params[:mail]
     @member.phone=params[:phone]
     @member.save
-    redirect_to("/member/#{params[:id]}/profile_edit")
+    redirect_to("/member/#{params[:id]}")
   end
 
   def start_time
     @member=Member.find(params[:id])
     @attendance = Attendance.new(work_date: Date.today, start_time: Time.now,member_id: @member.id)
     @attendance.save
+    redirect_to("/member/#{@member.id}")
   end
 
   def rest_start_time
     @member=Member.find(params[:id])
     @attendance = Attendance.new(work_date: Date.today, rest_start_time: Time.now,member_id: @member.id)
     @attendance.save
+    redirect_to("/member/#{@member.id}")
   end
   def rest_end_time
     @member=Member.find(params[:id])
     @attendance = Attendance.new(work_date: Date.today, rest_end_time: Time.now,member_id: @member.id)
     @attendance.save
+    redirect_to("/member/#{@member.id}")
   end
   def end_time
     @member=Member.find(params[:id])
     @attendance = Attendance.new(work_date: Date.today, end_time: Time.now,member_id: @member.id)
     @attendance.save
+    redirect_to("/member/#{@member.id}")
   end
   
 
